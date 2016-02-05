31e73f7627d8f39f9e3a72b2e05f009ef6ee36aa
Bart Wijnants <bartwijnants@outlook.com>
2016-02-05 21:03:14 +0100

    Added ui

---
+++ CleverClass.Ui/services/ClassGroupController.cs
@@ -0,0 +1,54 @@
+using CleverClass.Domain.Contract.Client;
+using CleverClass.Domain.Contract.Dto;
+using CleverClass.Domain.Contract.Interface;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace CleverClass.Domain.Rest
+{
+    [RoutePrefix("api/classgroup")]
+    public class ClassGroupController : ApiController
+    {
+        private readonly IClassGroupFacade _classGroupClient;
+        
+        public ClassGroupController()
+        {
+            _classGroupClient = new ClassGroupRestClient();
+        }
+        
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<ClassGroupDto> GetAll()
+        {
+            return _classGroupClient.GetAll();
+        }
+        
+        [HttpGet]
+        [Route("{id}")]
+        public ClassGroupDto Get(int id)
+        {
+            return _classGroupClient.Get(id);
+        }
+        
+        [HttpPost]
+        [Route("")]
+        public ClassGroupDto Create(ClassGroupDto classGroup)
+        {
+            return _classGroupClient.Create(classGroup);
+        }
+        
+        [HttpPut]
+        [Route("{id}")]
+        public ClassGroupDto Update(ClassGroupDto classGroup)
+        {
+            return _classGroupClient.Update(classGroup);
+        }
+        
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+            _classGroupClient.Delete(id);
+        }
+    }
+}
